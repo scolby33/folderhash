39cb0d35f197f4b97c79759ce940da0ba18fa4c1
Scott Colby <scolby33@gmail.com>
2017-08-08 00:22:57 -0700

    Change PyPI development status in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ META_PATH = os.path.join('src', 'folderhash', '__init__.py')
 KEYWORDS = []
 # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Framework :: AsyncIO',
     'Intended Audience :: Developers',
